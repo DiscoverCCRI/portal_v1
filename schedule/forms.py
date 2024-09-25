c0e819bb6780f64878bbc1b96541ce018f9dc143
BaileyMcCauslin <66763821+BaileyMcCauslin@users.noreply.github.com>
2024-09-25 12:31:24 -0700

    Bailey/experiment resource changes (#89)
    Revamped schedule management

--- schedule/forms.py
+++ schedule/forms.py
@@ -1,6 +1,27 @@
 from django import forms
+from resources.models import ResourceLocationChoice
 
-class DateTimeForm(forms.Form):
+class ScheduleForm(forms.Form):
     scheduled_time = forms.DateField(
-        widget=forms.DateInput(attrs={'type': 'date'})
+        widget=forms.DateInput(attrs={'type': 'date'}),
+        label="Schedule Time"
+    )
+
+    experiment_uuid = forms.UUIDField(
+        widget=forms.HiddenInput()
+    )
+
+class LocationFilterForm(forms.Form):
+    location = forms.ChoiceField(
+        choices=ResourceLocationChoice.choices(),
+        widget=forms.Select(),
+        required=False,
+        label="Site",
+    )
+
+class ExperimentSearchForm(forms.Form):
+    experiment_name = forms.CharField(
+        widget=forms.TextInput(attrs={"size": 60}),
+        required=False,
+        label="Search",
     )
--- schedule/views.py
+++ schedule/views.py
@@ -1,18 +1,69 @@
 from django.shortcuts import render, redirect
-from .forms import DateTimeForm
+from .forms import ScheduleForm, LocationFilterForm, ExperimentSearchForm
 from experiments.experiments import get_experiment_list
 from django.shortcuts import get_object_or_404
 from uuid import UUID
 from experiments.models import Experiment
 from .schedule import update_scheduled_time, change_experiment_state
 
+
 def schedule(request):
     experiments = get_experiment_list(request)
-    form = DateTimeForm()  # Initialize form
-    return render(
-        request,
-        "schedule.html", 
-        {"experiments": experiments, "form": form})  # Pass form to template
+    schedule_form = ScheduleForm()
+    location_form = LocationFilterForm()
+    search_form = ExperimentSearchForm()
+    
+    return render(request, "schedule.html", {
+        "experiments": experiments,
+        "schedule_form": schedule_form,
+        "location_form": location_form,
+        "search_form": search_form
+    })
+
+def schedule_experiment(request):
+    if request.method == "POST":
+        form = ScheduleForm(request.POST)
+        if form.is_valid():
+            scheduled_date = form.cleaned_data['scheduled_time']
+            experiment_uuid = form.cleaned_data['experiment_uuid']
+            experiment = get_object_or_404(Experiment, uuid=UUID(str(experiment_uuid)))
+            update_scheduled_time(experiment, scheduled_date)
+            change_experiment_state(experiment, 1)
+            return redirect('schedule')
+    return redirect('schedule')
+
+def site_filter(request):
+    experiments = Experiment.objects.all()
+    location_form = LocationFilterForm(request.POST or None)
+    search_form = ExperimentSearchForm()
+    
+    if location_form.is_valid():
+        location = location_form.cleaned_data['location']
+        experiments = experiments.filter(resources__location=location)
+
+    return render(request, "schedule.html", {
+        "experiments": experiments,
+        "schedule_form": ScheduleForm(),
+        "location_form": location_form,
+        "search_form": search_form
+    })
+
+def search_experiments(request):
+    experiments = Experiment.objects.all()
+    location_form = LocationFilterForm()
+    search_form = ExperimentSearchForm(request.POST or None)
+    
+    if search_form.is_valid():
+        name = search_form.cleaned_data['experiment_name']
+        experiments = experiments.filter(name__icontains=name)
+
+    return render(request, "schedule.html", {
+        "experiments": experiments,
+        "schedule_form": ScheduleForm(),
+        "location_form": location_form,
+        "search_form": search_form
+    })
+
 
 def move_to_error(request):
     if request.method == 'POST':
@@ -37,19 +88,3 @@ def move_to_not_scheduled(request):
         change_experiment_state( experiment, 0 )
     
     return redirect('schedule')
-
-def schedule_experiment(request):
-    if request.method == "POST":
-        form = DateTimeForm(request.POST)  # Bind form with POST data
-        if form.is_valid():
-            scheduled_date = form.data['scheduled_time']
-            experiment_uuid = form.data['experiment_uuid']
-            experiment = get_object_or_404(Experiment, 
-                                                uuid=UUID(str(experiment_uuid)))
-            update_scheduled_time(experiment,scheduled_date)
-            # Redirect to prevent re-submission
-            return redirect('schedule')
-    else:
-        form = DateTimeForm()  # If not POST, create a blank form
-
-    return render(request, "schedule.html", {"form": form})  # Pass form to template
